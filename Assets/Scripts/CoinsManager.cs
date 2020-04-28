d93341a2508804277ab7fb25710e860057b6d237
hamza herbou <hamza95herbou@gmail.com>
2020-04-28 15:40:21 +0000

    upload files

---
+++ Assets/Scripts/CoinsManager.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using System.Collections.Generic;
+
+public class CoinsManager : MonoBehaviour
+{
+	//References
+	[Header ("UI references")]
+	[SerializeField] TMP_Text coinUIText;
+	[SerializeField] GameObject animatedCoinPrefab;
+	[SerializeField] Transform target;
+
+	[Space]
+	[Header ("Available coins : (coins to pool)")]
+	[SerializeField] int maxCoins;
+	Queue<GameObject> coinsQueue = new Queue<GameObject> ();
+
+
+	[Space]
+	[Header ("Animation settings")]
+	[SerializeField] [Range (0.5f, 0.9f)] float minAnimDuration;
+	[SerializeField] [Range (0.9f, 2f)] float maxAnimDuration;
+
+	[SerializeField] Ease easeType;
+	[SerializeField] float spread;
+
+	Vector3 targetPosition;
+
+
+	private int _c = 0;
+
+	public int Coins {
+		get{ return _c; }
+		set {
+			_c = value;
+			//update UI text whenever "Coins" variable is changed
+			coinUIText.text = Coins.ToString ();
+		}
+	}
+
+	void Awake ()
+	{
+		targetPosition = target.position;
+
+		//prepare pool
+		PrepareCoins ();
+	}
+
+	void PrepareCoins ()
+	{
+		GameObject coin;
+		for (int i = 0; i < maxCoins; i++) {
+			coin = Instantiate (animatedCoinPrefab);
+			coin.transform.parent = transform;
+			coin.SetActive (false);
+			coinsQueue.Enqueue (coin);
+		}
+	}
+
+	void Animate (Vector3 collectedCoinPosition, int amount)
+	{
+		for (int i = 0; i < amount; i++) {
+			//check if there's coins in the pool
+			if (coinsQueue.Count > 0) {
+				//extract a coin from the pool
+				GameObject coin = coinsQueue.Dequeue ();
+				coin.SetActive (true);
+
+				//move coin to the collected coin pos
+				coin.transform.position = collectedCoinPosition + new Vector3 (Random.Range (-spread, spread), 0f, 0f);
+
+				//animate coin to target position
+				float duration = Random.Range (minAnimDuration, maxAnimDuration);
+				coin.transform.DOMove (targetPosition, duration)
+				.SetEase (easeType)
+				.OnComplete (() => {
+					//executes whenever coin reach target position
+					coin.SetActive (false);
+					coinsQueue.Enqueue (coin);
+
+					Coins++;
+				});
+			}
+		}
+	}
+
+	public void AddCoins (Vector3 collectedCoinPosition, int amount)
+	{
+		Animate (collectedCoinPosition, amount);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	//+n text
+	[SerializeField] GameObject coinNumPrefab;
+
+	CoinsManager coinsManager;
+
+	void Start ()
+	{
+		//find the CoinsManager
+		coinsManager = FindObjectOfType<CoinsManager> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//check if player collides with a coin
+		if (other.CompareTag ("coin")) {
+			//Add coins 7
+			coinsManager.AddCoins (other.transform.position, 7);
+
+			Destroy (other.gameObject);
+
+			//Show (+7) number
+			Destroy (Instantiate (coinNumPrefab, other.transform.position, Quaternion.identity), 1f);
+		}
+	}
+}
